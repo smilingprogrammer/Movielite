2fb3f19a3bb6942f32cf69d46f4bdd271b3d6ac7
smilingprogrammer <abdulsoburoyewale@gmail.com>
2021-12-30 09:18:21 +0100

    Added RatingBar

--- app/src/main/java/com/example/movielite/MainAdapter.kt
+++ app/src/main/java/com/example/movielite/MainAdapter.kt
@@ -4,6 +4,8 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
+import com.bumptech.glide.Glide
+import com.bumptech.glide.annotation.GlideModule
 import com.example.movielite.databinding.FragmentMainBinding
 import com.example.movielite.databinding.GridViewItemBinding
 import com.example.movielite.network.Movie
@@ -22,11 +24,16 @@ class MainAdapter(private val movies: List<Movie>): RecyclerView.Adapter<MainAda
         holder.bind(movies[position])
     }
 
+    @GlideModule
     inner class MainViewHolder(private val binding: GridViewItemBinding) :
         RecyclerView.ViewHolder(binding.root){
         fun bind(movie: Movie) {
+            Glide.with(itemView)
+                .load(movie.backdropPath)
+                .into(binding.marsImage)
             binding.marsImage.load(movie.backdropPath)
             binding.region.text = movie.title
+            binding.rating.rating = movie.voteAverage!!.div(2)
             /*binding.image.load(movie.image)*/
         }
     }
