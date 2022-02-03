c53d474a31887e52d60ffebc930f0eeaa6015f22
smilingprogrammer <abdulsoburoyewale@gmail.com>
2022-02-03 04:38:59 +0100

    Adding carousel

--- app/src/main/java/com/example/movielite/adapter/MainAdapter.kt
+++ app/src/main/java/com/example/movielite/adapter/MainAdapter.kt
@@ -1,13 +1,21 @@
 package com.example.movielite.adapter
 
+import android.content.Context
+import android.graphics.Rect
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.Px
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import coil.load
 import com.example.movielite.R
 import com.example.movielite.databinding.HorizontalScrollViewBinding
 import com.example.movielite.response.popularresponse.Movie
+import com.example.movielite.ui.MainFragment
+import java.lang.Math.abs
+import kotlin.math.roundToInt
 
 
 class MainAdapter (private val movies: List<Movie>, private val listener: (Movie) -> Unit
@@ -40,8 +48,120 @@ class MainAdapter (private val movies: List<Movie>, private val listener: (Movie
         }
     }
 
-
     override fun getItemCount(): Int {
         return movies.size
+    }
+}
+
+internal class ProminentLayoutManager(
+    context: MainFragment,
+
+    /**
+     * This value determines where items reach the final (minimum) scale:
+     * - 1f is when their center is at the start/end of the RecyclerView
+     * - <1f is before their center reaches the start/end of the RecyclerView
+     * - >1f is outside the bounds of the RecyclerView
+     * */
+    private val minScaleDistanceFactor: Float = 1.5f,
+
+    /** The final (minimum) scale for non-prominent items is 1-[scaleDownBy] */
+    private val scaleDownBy: Float = 0.5f
+) : LinearLayoutManager(context, HORIZONTAL, false) {
+
+    override fun onLayoutCompleted(state: RecyclerView.State?) =
+        super.onLayoutCompleted(state).also { scaleChildren() }
+
+    override fun scrollHorizontallyBy(
+        dx: Int,
+        recycler: RecyclerView.Recycler,
+        state: RecyclerView.State
+    ) = super.scrollHorizontallyBy(dx, recycler, state).also {
+        if (orientation == HORIZONTAL) scaleChildren()
+    }
+
+    private fun scaleChildren() {
+        val containerCenter = width / 2f
+
+        // Any view further than this threshold will be fully scaled down
+        val scaleDistanceThreshold = minScaleDistanceFactor * containerCenter
+
+        var translationXForward = 0f
+
+        for (i in 0 until childCount) {
+            val child = getChildAt(i)!!
+
+            val childCenter = (child.left + child.right) / 2f
+            val distanceToCenter = abs(childCenter - containerCenter)
+
+            val scaleDownAmount = (distanceToCenter / scaleDistanceThreshold).coerceAtMost(1f)
+            val scale = 1f - scaleDownBy * scaleDownAmount
+
+            child.scaleX = scale
+            child.scaleY = scale
+
+            val translationDirection = if (childCenter > containerCenter) -1 else 1
+            val translationXFromScale = translationDirection * child.width * (1 - scale) / 2f
+            child.translationX = translationXFromScale + translationXForward
+
+            translationXForward = 0f
+
+            if (translationXFromScale > 0 && i >= 1) {
+                // Edit previous child
+                getChildAt(i - 1)!!.translationX += 2 * translationXFromScale
+
+            } else if (translationXFromScale < 0) {
+                // Pass on to next child
+                translationXForward = 2 * translationXFromScale
+            }
+        }
+    }
+
+    override fun getExtraLayoutSpace(state: RecyclerView.State): Int {
+        // Since we're scaling down items, we need to pre-load more of them offscreen.
+        // The value is sort of empirical: the more we scale down, the more extra space we need.
+        return (width / (1 - scaleDownBy)).roundToInt()
+    }
+}
+
+/** Works best with a [LinearLayoutManager] in [LinearLayoutManager.HORIZONTAL] orientation */
+class LinearHorizontalSpacingDecoration(@Px private val innerSpacing: Int) :
+    RecyclerView.ItemDecoration() {
+
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        super.getItemOffsets(outRect, view, parent, state)
+
+        val itemPosition = parent.getChildAdapterPosition(view)
+
+        outRect.left = if (itemPosition == 0) 0 else innerSpacing / 2
+        outRect.right = if (itemPosition == state.itemCount - 1) 0 else innerSpacing / 2
+    }
+}
+
+/** Offset the first and last items to center them */
+class BoundsOffsetDecoration : RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        super.getItemOffsets(outRect, view, parent, state)
+
+        val itemPosition = parent.getChildAdapterPosition(view)
+
+        // It is crucial to refer to layoutParams.width (view.width is 0 at this time)!
+        val itemWidth = view.layoutParams.width
+        val offset = (parent.width - itemWidth) / 2
+
+        if (itemPosition == 0) {
+            outRect.left = offset
+        } else if (itemPosition == state.itemCount - 1) {
+            outRect.right = offset
+        }
     }
 }
--- app/src/main/java/com/example/movielite/ui/MainFragment.kt
+++ app/src/main/java/com/example/movielite/ui/MainFragment.kt
@@ -1,33 +1,35 @@
 package com.example.movielite.ui
 
-import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
+import androidx.core.view.doOnPreDraw
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.RecyclerView
-import androidx.viewpager2.widget.CompositePageTransformer
-import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.recyclerview.widget.*
 import androidx.viewpager2.widget.ViewPager2
 import com.example.movielite.R
 import com.example.movielite.adapter.MainAdapter
 import com.example.movielite.databinding.FragmentMainBinding
 import com.example.movielite.ViewModelFactory.MainViewModelFactory
+import com.example.movielite.adapter.BoundsOffsetDecoration
+import com.example.movielite.adapter.LinearHorizontalSpacingDecoration
+import com.example.movielite.adapter.ProminentLayoutManager
 import com.example.movielite.response.popularresponse.Movie
 import com.example.movielite.network.MovieApi
 import com.example.movielite.network.repository.MovieRepository
 import com.example.movielite.viewmodel.MainViewModel
-import kotlin.math.abs
 
 class MainFragment : Fragment() {
 
     private lateinit var viewPager2: ViewPager2
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
+    private lateinit var layoutManager: LinearLayoutManager
+    private lateinit var snapHelper: SnapHelper
     private lateinit var adapter: MainAdapter
 
     private var movies = mutableListOf<Movie>()
@@ -48,28 +50,24 @@ class MainFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         viewModel.popularMoviesLiveData.observe(/*this*/viewLifecycleOwner, {
             movies.addAll(it)
-            val adapter = MainAdapter(movies, viewPager2){
+            val adapter = MainAdapter(movies){
                 findNavController().navigate(R.id.action_mainFragment_to_movieDetailFragment,
                 bundleOf(ID_ARGS to it))
             }
 //            binding.show.layoutManager =
 //                StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.HORIZONTAL)
 //            binding.show.adapter = adapter
-            viewPager2 = binding.show
 
-            viewPager2.clipToPadding = false
-            viewPager2.clipChildren = false
-            viewPager2.offscreenPageLimit = 3
+            snapHelper = PagerSnapHelper()
+            layoutManager = ProminentLayoutManager(this)
 
-            viewPager2.getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+            val spacing = resources.getDimensionPixelSize(R.dimen.carousel_spacing)
+            addItemDecoration(LinearHorizontalSpacingDecoration(spacing))
+            addItemDecoration(BoundsOffsetDecoration())
 
-            val compositePageTransformer = CompositePageTransformer()
-            compositePageTransformer.addTransformer(MarginPageTransformer(20))
-            compositePageTransformer.addTransformer{ page, position ->
-                val r = 1 - abs(position)
-                page.scaleY = 0.85f + r * 0.25f
-            }
-            viewPager2.setPageTransformer(compositePageTransformer)
+            snapHelper.attachToRecyclerView(binding.show)
+
+            initRecyclerViewPosition(position)
 //
 //            val videoView = binding.comingSoonVideo
 //            val onlineUri = Uri.parse("")
@@ -78,6 +76,22 @@ class MainFragment : Fragment() {
 //            videoView.start()
             adapter.notifyDataSetChanged()
         })
+
+    }
+
+    private fun initRecyclerViewPosition(position: Int) {
+        // This initial scroll will be slightly off because it doesn't respect the SnapHelper.
+        // Do it anyway so that the target view is laid out, then adjust onPreDraw.
+        layoutManager.scrollToPosition(position)
+
+        binding.show.doOnPreDraw {
+            val targetView = layoutManager.findViewByPosition(position) ?: return@doOnPreDraw
+            val distanceToFinalSnap =
+                snapHelper.calculateDistanceToFinalSnap(layoutManager, targetView)
+                    ?: return@doOnPreDraw
+
+            layoutManager.scrollToPositionWithOffset(position, -distanceToFinalSnap[0])
+        }
     }
 
     companion object {
